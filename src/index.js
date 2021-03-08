bb476e6dffee03593bf66f67fcbe78d4a221625e
Philosopheya <78369694+Philosopheya@users.noreply.github.com>
2021-03-08 21:05:17 +0300

    fix: add three dots as spread operator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
 exports.min = function min (array) {
-  return !array || array.length === 0 ? 0 : Math.min(array);
+  return !array || array.length === 0 ? 0 : Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return !array || array.length === 0 ? 0 : Math.max(array);
+  return !array || array.length === 0 ? 0 : Math.max(...array);
 }
 
 exports.avg = function avg (array) {
